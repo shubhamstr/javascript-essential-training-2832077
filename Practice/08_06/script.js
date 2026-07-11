bff459451f48ff12e4cfa058e9ce228d794e9f5c
shubhamdev <shubhamsutar5799@gmail.com>
2026-07-11 17:19:29 +0530

    working with functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,18 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+function createArticle(data) {
+  const article = document.createElement('p');
+  article.textContent = `${data.title} - ${data.author}`;
+  document.getElementsByTagName('main')[0].appendChild(article);
+  return article;
+}
+
+createArticle({
+  title: 'The Importance of Learning JavaScript',
+  author: 'John Doe'
+});
+
+const p = document.getElementsByTagName('p')[0];
+p.classList.add('article');
